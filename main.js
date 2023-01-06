6212743b8e3edfb502e5c5d9c94a8a027f6f43a6
offbeat-dev <109253600+offbeat-dev@users.noreply.github.com>
2023-01-06 10:13:10 -0500

    add slides and find repetitive logic

--- main.js
+++ main.js
@@ -7,22 +7,56 @@ const slideWrapper = document.querySelector('.slides-wrapper');
 
 const slide1 = slides[0];
 const slide2 = slides[1];
+const slide3 = slides[2];
+const slide4 = slides[3];
 const slideMask1 = slideMasks[0];
 const slideMask2 = slideMasks[1];
+const slideMask3 = slideMasks[2];
+const slideMask4 = slideMasks[3];
+
+const num = 3;
 
 const doSomething = (progress) => {
+  //------------------------------------------------------------------
+  //---------------- SLIDE 1
+
   slide1.style.transform = `translate(0,-${
-    progress * slideWrapper.offsetHeight
+    progress * slideWrapper.offsetHeight * num
   }px)`;
-  slideMask1.style.transform = `translate(0,${
-    progress * slideWrapper.offsetHeight
-  }px)`;
+
+  slideMask1.style.transform = `translate(0, calc(-${
+    slideWrapper.offsetHeight * 0
+  }px + ${progress * slideWrapper.offsetHeight * num}px))`;
+
+  //------------------------------------------------------------------
+  //---------------- SLIDE 2
   slide2.style.transform = `translate(0,-${
-    progress * slideWrapper.offsetHeight
+    progress * slideWrapper.offsetHeight * num
   }px)`;
-  slideMask2.style.transform = `translate(0,calc(-${
-    slideWrapper.offsetHeight
-  }px + ${progress * slideWrapper.offsetHeight}px))`;
+
+  slideMask2.style.transform = `translate(0, calc(-${
+    slideWrapper.offsetHeight * 1
+  }px + ${progress * slideWrapper.offsetHeight * num}px))`;
+
+  //------------------------------------------------------------------
+  //---------------- SLIDE 3
+  slide3.style.transform = `translate(0,-${
+    progress * slideWrapper.offsetHeight * num
+  }px)`;
+
+  slideMask3.style.transform = `translate(0,calc(-${
+    slideWrapper.offsetHeight * 2
+  }px + ${progress * slideWrapper.offsetHeight * num}px))`;
+
+  //------------------------------------------------------------------
+  //---------------- SLIDE 4
+  slide4.style.transform = `translate(0,-${
+    progress * slideWrapper.offsetHeight * num
+  }px)`;
+
+  slideMask4.style.transform = `translate(0,calc(-${
+    slideWrapper.offsetHeight * 3
+  }px + ${progress * slideWrapper.offsetHeight * num}px))`;
 };
 
 const lenis = new Lenis({
@@ -39,8 +73,6 @@ const lenis = new Lenis({
 
 //get scroll value
 lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
-  console.log({ scroll, limit, velocity, direction, progress });
-
   doSomething(progress);
 });
 
